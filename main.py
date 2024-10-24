5dc297d73203c2abda7b0dbe78b6742c49ecbcb0
ba-dor <bavo.vandorpe@gmail.com>
2024-10-24 21:56:30 +0200

    implemented get_char_count function to return a dictionary with the number of occurances of a certain character

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ def get_word_count(file_contents):
     word_count = len(split_file_contents)
     return word_count
 
+def get_char_count(file_contents):
+    character_count = {}
+    file_contents_lower = file_contents.lower()
+    print(file_contents_lower)
+    file_contents_words = file_contents_lower.split()
+    for i in range(len(file_contents_words)):
+        for char in file_contents_words[i]:
+            if char in character_count:
+                character_count[char] += 1
+            else:
+                character_count[char] = 1
+    return character_count
+    # sort:
+    # return dict(sorted(character_count.items()))
+
 f = open('books/frankenstein.txt', 'r')
 file_contents = f.read()
-word_count = get_word_count(file_contents)
-print(word_count)
+print(get_char_count(file_contents))
