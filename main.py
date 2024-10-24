4881a6cd3d9dfa11682c102f681d8b68f4583756
ba-dor <bavo.vandorpe@gmail.com>
2024-10-24 08:55:02 +0200

    add word count

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 f = open('books/frankenstein.txt', 'r')
 file_contents = f.read()
-print(file_contents)
+file_contents_split = file_contents.split()
+word_count = 0
+word_count = len(file_contents_split)
+print(word_count)
