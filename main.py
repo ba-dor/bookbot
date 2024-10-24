09f260dedd12e1ae761ff4d50d6a1964b03260f3
ba-dor <bavo.vandorpe@gmail.com>
2024-10-24 09:00:05 +0200

    Moved word counting to seperate function get_word_count

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def get_word_count(file_contents):
+    word_count = 0
+    split_file_contents = file_contents.split()
+    word_count = len(split_file_contents)
+    return word_count
+
 f = open('books/frankenstein.txt', 'r')
 file_contents = f.read()
-file_contents_split = file_contents.split()
-word_count = 0
-word_count = len(file_contents_split)
+word_count = get_word_count(file_contents)
 print(word_count)
