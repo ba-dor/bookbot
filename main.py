0e5139847de4a6ab03b70ef060c7703a4c39ca5a
ba-dor <bavo.vandorpe@gmail.com>
2024-10-26 13:08:37 +0200

    Added printing report to the console

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def get_word_count(file_contents):
 def get_char_count(file_contents):
     character_count = {}
     file_contents_lower = file_contents.lower()
-    print(file_contents_lower)
     file_contents_words = file_contents_lower.split()
     for i in range(len(file_contents_words)):
         for char in file_contents_words[i]:
@@ -15,10 +14,19 @@ def get_char_count(file_contents):
                 character_count[char] += 1
             else:
                 character_count[char] = 1
-    return character_count
-    # sort:
+    # Unsorted:
+    # return character_count
+    # Sort alphabetically:
     # return dict(sorted(character_count.items()))
+    # Sort by value:
+    return dict(sorted(character_count.items(),key=lambda x:x[1], reverse=True))
 
-f = open('books/frankenstein.txt', 'r')
+book_path = 'books/frankenstein.txt'
+f = open(book_path, 'r')
 file_contents = f.read()
-print(get_char_count(file_contents))
+char_count = get_char_count(file_contents)
+
+print(f"--- Begin report of {book_path} ---")
+print(f"{get_word_count(file_contents)} words found in the document\n")
+for item in char_count:
+    print(f"The '{item}' character was found {char_count[item]} times")
